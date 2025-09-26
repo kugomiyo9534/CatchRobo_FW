64917445fa4c6cc2e0dc72746537c6000cb7acbb
kugomiyo9534 <120617847+kugomiyo9534@users.noreply.github.com>
2025-09-27 06:03:24 +0900

    add: wip config, main

---
+++ include/config.hpp
@@ -0,0 +1,26 @@
+
+namespace Ethernet {
+
+}
+
+namespace CAN1_FD {
+constexpr uint8_t CTX1_PIN 22;
+constexpr uint8_t CRX1_PIN 23;
+} // namespace CAN1_FD
+
+namespace CAN2 {
+constexpr uint8_t CTX2_PIN 0;
+constexpr uint8_t CRX2_PIN 1;
+} // namespace CAN2
+
+namespace CAN3 {
+constexpr uint8_t CTX3_PIN 29;
+constexpr uint8_t CRX3_PIN 30;
+} // namespace CAN3
+
+namespace SPI0 {
+constexpr uint8_t CS_PIN   10;
+constexpr uint8_t MOSI_PIN 11;
+constexpr uint8_t MISO_PIN 12;
+constexpr uint8_t SCK_PIN 13;
+} // namespace SPI0
